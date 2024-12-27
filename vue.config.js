1bc6299a5ebb5096dee89f98453b159ac8569eb2
zhiraffik <zhiraff2004@live.com>
2024-12-27 19:12:46 +0500

    указан верный путь

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/FakeSite-Store/',
+  publicPath: `/FakeSite-Store/`,
   transpileDependencies: true
 })
